5251ff4dcb4c357048cbb0f7f9c2233054c35b5f
Ashutosh Bhargava <ashutumho@gmail.com>
2021-06-13 15:57:58 +0530

    working GUI and data collection

--- eyeCommGUI.py
+++ eyeCommGUI.py
@@ -10,13 +10,14 @@ from PIL import Image, ImageTk
 ##from eyeTrackerVideoClass import eyesRects
 import eyeTrackerVideoClass
 count = 0
+numImg = 100
 class eyeComm(eyeTrackerVideoClass.eyesRects):
     def __init__(self,path='.\\'):
         eyeTrackerVideoClass.eyesRects.__init__(self)
         self.os = os
         self.path = path
         self.makingDir()
-              
+     
         self.root = tk.Tk()
         self.root.title("eye-Comm")
         self.root.protocol('WM_DELETE_WINDOW', self.destructor)
@@ -24,15 +25,24 @@ class eyeComm(eyeTrackerVideoClass.eyesRects):
         self.panel = tk.Label(self.root)  # initialize image panel
         self.panel.pack(anchor=tk.SE,side=tk.BOTTOM, fill='none', expand=tk.FALSE)#(padx=10, pady=100)
          
+##        self.s = tk.ttk.Style()
+##        self.bg = self.s.lookup('TFrame', 'background')
+        
         self.wButton = 10
         self.hButton = 3        
         self.width   = self.root.winfo_screenwidth()
         self.height  = self.root.winfo_screenheight()
+        
         self.root.geometry('%sx%s' % (int(self.width/2), int(self.height/2)))
+        self.Canvas = tk.Canvas(self.root,width=int(self.width/2), height=int(self.height/2),borderwidth=0,highlightthickness=0)
+        self.bg = self.Canvas['bg']
+        print(self.bg)
+        self.Canvas.pack()
         
         self._btnState = 'Play'
         self.playPauseBTN = tk.Button(self.root,text='Play',   command=self.playMe, bg='green',   width=self.wButton, height=self.hButton)
         self.playPauseBTN.place( relx=0.5,  rely=1.0, anchor=tk.S )
+        self.imageLoad()
 ##
 ##        self.btnLeft   = tk.Button(self.root,text="Left",   command=lambda: self.BottonPress(self.leftLookPath),   width=self.wButton, height=self.hButton)
 ##        self.btnCenter = tk.Button(self.root,text="Center", command=lambda: self.BottonPress(self.centerLookPath), width=self.wButton, height=self.hButton)
@@ -42,13 +52,14 @@ class eyeComm(eyeTrackerVideoClass.eyesRects):
 ##        self.btnLeft.place(   relx=0.35, rely=1.0, anchor=tk.SW)
 ##        self.btnCenter.place( relx=0.5,  rely=1.0, anchor=tk.S )
 ##        self.btnRight.place(  relx=0.65, rely=1.0, anchor=tk.SE)
-        self.imageLoad()
+        
         
     def playMe(self):
 
         if self.playPauseBTN['text'] == 'Stop':
             self.playPauseBTN['text'] = 'Play'
             self.playPauseBTN['bg'] = 'green'
+            self.playPauseBTN.place( relx=0.5,  rely=1.0, anchor=tk.S )
            
         else:
             self.playPauseBTN['text'] = 'Stop'
@@ -88,16 +99,33 @@ class eyeComm(eyeTrackerVideoClass.eyesRects):
         
     def BottonPress(self): 
         # saving the image data in Left folder when Left button press
-        global count
-        print(count,'Bingo')
+        global count,numImg
+##        print(count,'Bingo')
         count += 1
-        
-        if count>=10:
+        if count==int(numImg/3) or count == int(numImg*2/3) :
+            self.Canvas.delete('all')
+
+        if count < int(numImg/3):
+            self.Canvas.create_rectangle(0,0,int(self.width/6),int(self.height/5),fill="#fb0")#outline="#fb7",
+            self.cv2.imwrite(self.os.path.join(self.leftLookPath,self.filename),self.img)
+
+        elif count >= int(numImg/3) and count < int(numImg*2/3):
+            self.Canvas.create_rectangle(int(self.width/6),0,int(self.width*2/6),int(self.height/5),fill="#fb0")
+            self.cv2.imwrite(self.os.path.join(self.centerLookPath,self.filename),self.img)
+
+        elif count >=int(numImg*2/3):
+            self.Canvas.create_rectangle(int(self.width*2/6),0,int(self.width*3/6)-1,int(self.height/5),fill="#fb0")
+            self.cv2.imwrite(self.os.path.join(self.rightLookPath,self.filename),self.img)
+
+        self.Canvas.pack()
+        if count>=numImg:
             self.root.after_cancel(self._btnState)
             self.playMe()
             count = 0
+            self.Canvas.delete('all')
+            
         else:
-            self._btnState = self.root.after(30, self.BottonPress)
+            self._btnState = self.root.after(10, self.BottonPress)
         
 ##        path = path
 ##        print(path+'\\'+self.filename)
